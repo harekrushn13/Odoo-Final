c1ab0a5b6a40a28f2008c9246f0b1ff688bd09b3
devamdoshi212 <devamdoshi212@gmail.com>
2024-07-14 10:25:20 +0530

    detail book page route added

--- client-web/src/components/BookCard.jsx
+++ client-web/src/components/BookCard.jsx
@@ -1,36 +1,44 @@
+import { Navigate, useNavigate } from "react-router-dom";
+
 function BookCard({ book }) {
-
-    return (
-        <div
-            key={book.id}
-            className="flex rounded-lg border border-gray-300 bg-white shadow-lg overflow-hidden transition-transform transform hover:scale-105 p-4"
+  const navigate = useNavigate();
+  return (
+    <div
+      key={book.id}
+      className="flex rounded-lg border border-gray-300 bg-white shadow-lg overflow-hidden transition-transform transform hover:scale-105 p-4"
+    >
+      <img
+        src={
+          book.volumeInfo.imageLinks?.thumbnail ||
+          "https://via.placeholder.com/150"
+        }
+        alt={book.volumeInfo.title}
+        className="w-1/3 h-48 object-contain"
+      />
+      <div className="p-4 flex flex-col justify-between w-2/3">
+        <h5 className="text-lg font-semibold text-blue-gray-900">
+          {book.volumeInfo.title}
+        </h5>
+        <p className="text-sm text-gray-600">
+          {book.volumeInfo.averageRating
+            ? `Rating: ${book.volumeInfo.averageRating}`
+            : "Rating: N/A"}
+        </p>
+        <p className="text-base font-light text-gray-700 truncate">
+          {book.volumeInfo.description
+            ? book.volumeInfo.description.slice(0, 100) + "..."
+            : "No description available."}
+        </p>
+        <button
+          className="mt-4 rounded-lg bg-gray-900 py-2 px-4 text-white text-sm font-bold uppercase shadow-md hover:shadow-lg focus:opacity-85"
+          type="button"
+          onClick={() => navigate(`/book/${book.id}`)}
         >
-            <img
-                src={book.volumeInfo.imageLinks?.thumbnail || "https://via.placeholder.com/150"}
-                alt={book.volumeInfo.title}
-                className="w-1/3 h-48 object-contain"
-            />
-            <div className="p-4 flex flex-col justify-between w-2/3">
-                <h5 className="text-lg font-semibold text-blue-gray-900">
-                    {book.volumeInfo.title}
-                </h5>
-                <p className="text-sm text-gray-600">
-                    {book.volumeInfo.averageRating ? `Rating: ${book.volumeInfo.averageRating}` : "Rating: N/A"}
-                </p>
-                <p className="text-base font-light text-gray-700 truncate">
-                    {book.volumeInfo.description ? book.volumeInfo.description.slice(0, 100) + '...' : 'No description available.'}
-                </p>
-                <button
-                    className="mt-4 rounded-lg bg-gray-900 py-2 px-4 text-white text-sm font-bold uppercase shadow-md hover:shadow-lg focus:opacity-85"
-                    type="button"
-                    onClick={() => navigate(`/book/${book.id}`)}
-                >
-                    View Details
-                </button>
-            </div>
-        </div>
-    )
+          View Details
+        </button>
+      </div>
+    </div>
+  );
 }
-
 
 export default BookCard;
